7816f03b156fea2b0e4bd8ba2e6aabef8a8e6eb7
joaopaulocosta551 <kenocosta44@gmail.com>
2025-09-12 21:25:48 -0300

    fix: remove duplicate declaration of leadStatuses constant

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,7 @@
+
 import type { Lead } from "./types";
 
 export const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-
 export const leadStatuses: Lead['status'][] = ['New', 'Contacted', 'Qualified', 'Disqualified'];
+
